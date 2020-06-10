ad59a0c8e7a1e664cbc0a0c7cea1803aea699bda
Fabio Alberto Mayorga Duarte <fabiomayorgad@hotmail.com>
2020-06-10 17:44:06 -0500

    ok

--- src/app/theme/admin-layout/sidebar/user-panel.component.ts
+++ src/app/theme/admin-layout/sidebar/user-panel.component.ts
@@ -23,12 +23,12 @@ export class UserPanelComponent implements OnInit {
 
   ngOnInit() {
     const helper = new JwtHelperService();
-    // this.local = localStorage.getItem('currentUser');
-    // this.local = JSON.parse(this.local);
-    // this.decodedToken = helper.decodeToken(this.local.token);
-    this.username = 'Superadministrador'; //this.decodedToken.name;
-    this.email = 'super@dominussalud.co'; //this.decodedToken.email;
-    this.role = 'Superadministrador'; //this.decodedToken.rolename ? this.decodedToken.rolename : this.decodedToken.role;
+     this.local = localStorage.getItem('currentUser');
+     this.local = JSON.parse(this.local);
+     this.decodedToken = helper.decodeToken(this.local.token);
+    this.username = this.decodedToken.name;
+    this.email = this.decodedToken.email;
+    this.role = this.decodedToken.rolename ? this.decodedToken.rolename : this.decodedToken.role;
     this.last = 'Marzo 18 de 2019, 10:00 a.m.';
     this.ip = '192.168.25.69';
   }
