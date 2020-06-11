42e77374fec6996b7c724aebcfd17fa6a5fc6008
Fabio Mayorga <fabiomayorgad93@gmail.com>
2020-06-11 18:13:27 -0500

    server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'http://198.211.112.240',
+  apiUrl: 'localhost',
   // apiUrl: 'http://esculappio.local:3000',
   apiBaseMain: {
   main: 'main',
